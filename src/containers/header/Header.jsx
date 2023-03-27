d0a26bdd30dc9dfa52007504075960e1b8dc5ca7
rendi <Rendipur234@gmail.com>
2023-03-28 03:39:32 +0700

    make responsive header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque alias provident officiis eius cum, consequuntur mollitia dolorem. Eos veniam impedit, dolorem sint iste quaerat porro unde maxime fugit temporibus alias.</p>
 
-export default Header
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1.600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+        <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+    </div>
+  );
+};
+
+export default Header;
